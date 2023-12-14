973ced47e9257b3b6d8e77db10d8034e7ccc9ace
DanyPankrat <140436906+DanyPankrat@users.noreply.github.com>
2023-12-14 02:57:30 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,13 @@
 def main():
-
-    for item in data:
-        print(f"Random Number: {item}")
-
-    data = generate_random_data()
-    main()
-    data = [random.randint(1, 100) for _ in range(10)]
     return data
+    data = generate_random_data()
+
+
 def generate_random_data():
-import random
+        print(f"Random Number: {item}")
 
 if __name__ == "__main__":
     for item in data:
-def main():
-        print(f"Random Number: {item}")
-
-
-    main()
-if __name__ == "__main__":
-
-import random
     data = [random.randint(1, 100) for _ in range(10)]
-
-def generate_random_data():
-    return data
-    data = generate_random_data()
+    main()
+import random
