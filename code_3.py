f276144530944cb6c9493b5dbede31053da613e3
DanyPankrat <140436906+DanyPankrat@users.noreply.github.com>
2023-12-16 21:58:24 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,40 +1,65 @@
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
+    main()
+def generate_random_data():
+def main():
+
+    return data
+import random
 
     for item in data:
-    main()
-def main():
-    return data
+
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()    data = generate_random_data()
 
         print(f"Random Number: {item}")
 if __name__ == "__main__":
-import random
-def generate_random_data():
-
-    return data
-def generate_random_data():
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    data = generate_random_data()
+    for item in data:
 def main():
 
+    main()
+
+def generate_random_data():
+import random
+    return data
     data = [random.randint(1, 100) for _ in range(10)]
     main()
 
-
+    return data
 import random
+if __name__ == "__main__":
+
+
+def main():
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
     for item in data:
+    data = generate_random_data()
+def generate_random_data():
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
+
     for item in data:
+if __name__ == "__main__":
+def main():
+import random
+def generate_random_data():
+        print(f"Random Number: {item}")
     main()
 
 
-def generate_random_data():
-def main():
-    return data
-import random
         print(f"Random Number: {item}")
-    data = generate_random_data()
+    main()
+import random
 
+def generate_random_data():
 
+    return data
     data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
+    data = generate_random_data()
+def main():
+    for item in data:
